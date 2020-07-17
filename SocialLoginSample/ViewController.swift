b6bc0680d4e044684ef9abac414bdc47245b1610
Mahesh Vanam <maheshvanam09@gmail.com>
2020-07-17 12:36:26 +0530

    Added. README.md file to git

---
+++ SocialLoginSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLoginSample
+//
+//  Created by admin on 17/07/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
