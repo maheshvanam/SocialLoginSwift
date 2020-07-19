8d33f30820ba8cfc295c0d5588058c29da306f26
Mahesh Vanam <maheshvanam09@gmail.com>
2020-07-19 15:31:28 +0530

    Add. added fbLogin button and integrated fbLoginSdk

--- SocialLoginSample/AppDelegate.swift
+++ SocialLoginSample/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import GoogleSignIn
+import FBSDKCoreKit
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- SocialLoginSample/ViewController.swift
+++ SocialLoginSample/ViewController.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 import GoogleSignIn
-
+import FBSDKLoginKit
 class ViewController: UIViewController  {
     
-   @IBOutlet weak var signInButton: GIDSignInButton!
-
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    let fbSignInButton = FBLoginButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         GIDSignIn.sharedInstance()?.presentingViewController = self
         GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        fbSignInButton.center = view.center
+        view.addSubview(fbSignInButton)
+
+        if let token = AccessToken.current,
+            !token.isExpired {
+            print("User is logged in, do work such as go to next view controller.")
+        }
+        fbSignInButton.permissions =  ["public_profile", "email"]
     }
 }
 
